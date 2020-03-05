01d3f0cf640f8d144894909405d621c0966656ff
ericbanker12 <ericbanker12@gmail.com>
2020-03-05 08:14:19 -0600

    add memory helpers

--- dllmain.cpp
+++ dllmain.cpp
@@ -10,6 +10,47 @@ using namespace loader;
 
 nlohmann::json ConfigFile;
 
+// https://stackoverflow.com/a/55030118
+DWORD FindProcessId(const std::wstring& processName)
+{
+    PROCESSENTRY32W processInfo;
+    processInfo.dwSize = sizeof(processInfo);
+
+    HANDLE processesSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+    if (processesSnapshot == INVALID_HANDLE_VALUE) {
+        return 0;
+    }
+
+    Process32FirstW(processesSnapshot, &processInfo);
+    if (!processName.compare(processInfo.szExeFile))
+    {
+        CloseHandle(processesSnapshot);
+        return processInfo.th32ProcessID;
+    }
+
+    while (Process32NextW(processesSnapshot, &processInfo))
+    {
+        if (!processName.compare(processInfo.szExeFile))
+        {
+            CloseHandle(processesSnapshot);
+            return processInfo.th32ProcessID;
+        }
+    }
+
+    CloseHandle(processesSnapshot);
+    return 0;
+}
+
+DWORD_PTR FindPointerAddress(HANDLE phandle, DWORD_PTR ptr, DWORD_PTR offsets[], int n)
+{
+    DWORD_PTR ptrAddress;
+    for (int i = 0; i < n; i++) {
+        ReadProcessMemory(phandle, (LPCVOID)ptr, &ptrAddress, sizeof(ptrAddress), 0);
+        ptr = ptrAddress + offsets[i];
+    }
+    return ptr;
+}
+
 void onLoad()
 {
     LOG(INFO) << "FoV Changer Loading...";
@@ -24,6 +65,16 @@ void onLoad()
 
     config >> ConfigFile;
     LOG(INFO) << "FoV Changer: Found config file";
+
+    DWORD procID = FindProcessId(L"MonsterHunterWorld.exe");
+    HANDLE phandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, procID);
+    DWORD_PTR fovPointer = 0x0;
+    DWORD_PTR fovPointerOffsets[] = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
+    DWORD_PTR fovAddress = FindPointerAddress(phandle, fovPointer, fovPointerOffsets, 7);
+    float fovValue;
+    ReadProcessMemory(phandle, (LPCVOID)fovAddress, &fovValue, sizeof(fovValue), 0);
+    fovValue = fovValue * 1.2;
+    WriteProcessMemory(phandle, (LPVOID)fovAddress, &fovValue, sizeof(fovValue), 0);
 
     LOG(INFO) << "DONE !";
 }
